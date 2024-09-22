9fe930dbab5e84b2e4f24c3e560860b60592ca26
isarochaa <163015912+isarochaa@users.noreply.github.com>
2024-09-22 18:52:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior pais do mundo em extensão territorial?',
+    'Rússia'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou a obra "Mona Lisa"?',
+    'leonardo da Vinci'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o valor de x na equação (2x + 3 = 9?',
+    'x = 3'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o principal gás que as plantas absorvem durante a fotossíntese?',
+    'Dióxido de carbono (CO2)'
+)
